3e6c00b2e0be5fc793191b0f35e4724233c8a8ad
eappiah3 <eappiah640@gmail.com>
2026-04-29 06:05:52 +0000

    Assignment 7

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static(__dirname));
 const db = mysql.createConnection({
   host: '127.0.0.1',
   user: 'appuser',
-  password: 'appuser123',
+  password: 'appUser123!',
   database: 'CS3110project'
 });
 
