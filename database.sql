470e5f115f9fcdc19a62dcc452ec35013e781be0
eappiah3 <eappiah640@gmail.com>
2026-05-01 06:43:32 +0000

    Update database.sql code

--- database.sql
+++ database.sql
@@ -35,7 +35,6 @@ INSERT INTO users (username, password, role)
   ('admin', 'adminPass1!', 'admin'),
   ('author', 'authorPass1!', 'author');
 
--- Fixed: password now matches server.js connection password
 CREATE USER 'appuser'@'%' IDENTIFIED BY 'appUser123!';
 GRANT ALL PRIVILEGES ON `CS3110project`.* TO 'appuser'@'%';
 FLUSH PRIVILEGES;
