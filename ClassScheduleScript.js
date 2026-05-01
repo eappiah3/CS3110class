028c5d564fb3f785790b23b70fe6e6f8acee4e7e
eappiah3 <143494893+eappiah3@users.noreply.github.com>
2026-05-01 02:14:22 -0600

    Update ClassScheduleScript.js

--- ClassScheduleScript.js
+++ ClassScheduleScript.js
@@ -27,12 +27,6 @@ async function requestNotificationPermission() {
   }
 }
 
-function showNotification(title, body) {
-  if (Notification.permission === "granted") {
-    new Notification(title, { body });
-  }
-}
-
 /* =======================
    LOAD CLASSES (WITH CACHE)
 ======================= */
