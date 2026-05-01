9a1a47115657db86f4bff91520153ff0ab23f740
eappiah3 <143494893+eappiah3@users.noreply.github.com>
2026-05-01 02:07:43 -0600

    Update ClassScheduleScript.js

--- ClassScheduleScript.js
+++ ClassScheduleScript.js
@@ -133,6 +133,14 @@ form.addEventListener('submit', async (e) => {
   }
 });
 
+function showNotification(title, body) {
+  console.log("Permission:", Notification.permission);
+  console.log("Trying to show:", title, body);
+  if (Notification.permission === "granted") {
+    new Notification(title, { body });
+  }
+}
+
 /* =======================
    CLEAR CACHE (OPTIONAL)
 ======================= */
